129322579d8f61875c9ee5ede750c4b87e53cf49
Darkstalker17 <dakshithkarthik.a@gmail.com>
2025-05-26 10:27:25 +0400

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,4 @@
+num = int(input("Enter a number: "))
+n = int(input("Enter the the power which you need to raise the number to: "))
+power = num**n
+print("The nth power of the number",num,"is",power)
